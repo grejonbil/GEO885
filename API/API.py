621e43810628c874c06c6fde4c3942e63a6191b7
grbiuzh <gregory.biland@uzh.ch>
2022-04-01 15:12:28 +0200

    Update

--- API/API.py
+++ API/API.py
@@ -11,10 +11,6 @@ def retrieve_emissions(origin, destination, cabin_class, currencies):
              "cabin_class": cabin_class,
              "currencies": [currencies]})
 
-    # Peter: I am still not sure what the following lines do and why they are necessary
-    # Seems you have to get things into a format that go climate understands, but why do you use
-    # a for loop here when there is always only one segment per flight? Or is that different in the full data set?
-
     payload = {}
     for index, segment in enumerate(data["segments"]):
         origin = segment["origin"]
@@ -38,14 +34,9 @@ def retrieve_emissions(origin, destination, cabin_class, currencies):
 
     return footprint
 
-
-# I had problems reading the csv file
-# The first column does not have a header, which caused pd.read_csv to miss-align columns and column headers
-# e.g. the column with departure airports had the column header "cabin_class"
-# "Test_Data_peter.csv" has these issues fixed
 # testCSV = pd.read_csv("Test_Data_peter.csv")
 amm_test = pd.read_csv("/Users/chaualala/Desktop/UZH/MSc Geographie/2. Semester/GEO885 - GIS Science Project/GEO885/R/amm_test.csv")
-# amm_test = pd.read_csv("/Users/chaualala/Desktop/UZH/MSc Geographie/2. Semester/GEO885 - GIS Science Project/GEO885/R/amm_test.csv")
+# amm = pd.read_csv("/Users/chaualala/Desktop/UZH/MSc Geographie/2. Semester/GEO885 - GIS Science Project/GEO885/R/amm.csv")
 
 amm_test['EMISSIONS_KGCO2EQ'] = amm_test.apply(lambda x: retrieve_emissions(origin=x.DEPARTURE_AIRPORT,
                                                                   destination=x.ARRIVAL_AIRPORT,
