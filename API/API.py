6cce03144565b49f9dedba55b7a9e80202ccb2cb
grbiuzh <gregory.biland@uzh.ch>
2022-04-09 09:57:59 +0200

    Final code update

--- API/API.py
+++ API/API.py
@@ -1,16 +1,14 @@
 # API
-from curses import ERR
 import json
-from typing import List
+from math import nan
 import pandas as pd
 import requests
 import csv
 import time
-import re
 
 tic = time.perf_counter()
 
-def retrieve_IATA(FN_IATA, FN):
+def retrieve_IATA_dep(FN_IATA, FN):
     # Key = 32c0d6-dbc517
     # Key2 = 23bd52-8dbdd9
     payload = ({"airlineIata":FN_IATA, "flightNumber": FN})
@@ -25,25 +23,38 @@ def retrieve_IATA(FN_IATA, FN):
     print(answer.text)
     try:
         dep_code = json.loads(answer.text)[0]["departureIata"]
-        arr_code = json.loads(answer.text)[0]["arrivalIata"]
 
     except KeyError:
         dep_code = None
+
+    return dep_code
+
+def retrieve_IATA_arr(FN_IATA, FN):
+    # Key = 32c0d6-dbc517
+    # Key2 = 23bd52-8dbdd9
+    payload = ({"airlineIata":FN_IATA, "flightNumber": FN})
+
+    # https://aviation-edge.com/v2/public/routes?key=23bd52-8dbdd9&airlineIata=LX&flightNumber=15 -> EXAMPLE
+
+    answer = requests.get(
+        'http://aviation-edge.com/v2/public/routes?key=23bd52-8dbdd9',
+        params=payload,
+    )
+
+    print(answer.text)
+    try:
+        arr_code = json.loads(answer.text)[0]["arrivalIata"]
+
+    except KeyError:
         arr_code = None
 
-    return dep_code, arr_code
+    return arr_code
 
 
 def column_change(file):
-    code_list = []
-    code_list = pd.DataFrame(file['IATA_CODE'].tolist()).add_prefix("col")
-
-    # file['IATA_CODE'] = pd.Series(file['IATA_CODE'], dtype="string")
-    # file['IATA_CODE'] = str(file['IATA_CODE']).replace('(','').replace(')','')
-
-    # file["DEPARTURE_AIRPORT_CODE"], file["ARRIVAL_AIRPORT_CODE"] = file['IATA_CODE'].str.split(',', 1).str
-    print(code_list)
-    return code_list
+    file.DEPARTURE_AIRPORT.fillna(file.IATA_CODE_DEP,inplace=True)
+    file.ARRIVAL_AIRPORT.fillna(file.IATA_CODE_ARR,inplace=True)
+    return 
 
 def retrieve_emissions(origin, destination, cabin_class, currencies):
     data = ({"segments": [{"origin": origin,
@@ -78,18 +89,19 @@ def retrieve_emissions(origin, destination, cabin_class, currencies):
 amm_test = pd.read_csv("/Users/chaualala/Desktop/UZH/MSc Geographie/2. Semester/GEO885 - GIS Science Project/GEO885/R/amm_test.csv")
 # amm = pd.read_csv("/Users/chaualala/Desktop/UZH/MSc Geographie/2. Semester/GEO885 - GIS Science Project/GEO885/R/amm.csv")
 
-amm_test["IATA_CODE"] = amm_test.apply(lambda x: retrieve_IATA(FN_IATA = x.fn_code, FN=x.fn_number), axis=1) 
+amm_test["IATA_CODE_DEP"] = amm_test.apply(lambda x: retrieve_IATA_dep(FN_IATA = x.fn_code, FN=x.fn_number), axis=1) 
+amm_test["IATA_CODE_ARR"] = amm_test.apply(lambda x: retrieve_IATA_arr(FN_IATA = x.fn_code, FN=x.fn_number), axis=1) 
 
-column_change(amm_test)
+column_change(amm_test) #Transfers all values from the API column to the missing NA values in the columns DEPARTURE_AIRPORT and ARRIVAL_AIRPORT
 
-# amm_test['EMISSIONS_KGCO2EQ'] = amm_test.apply(lambda x: retrieve_emissions(origin=x.DEPARTURE_AIRPORT,
-#                                                                   destination=x.ARRIVAL_AIRPORT,
-#                                                                   cabin_class=x.cabin_class,
-#                                                                   currencies=x.currencies), axis=1)
+amm_test['EMISSIONS_KGCO2EQ'] = amm_test.apply(lambda x: retrieve_emissions(origin=x.DEPARTURE_AIRPORT,
+                                                                  destination=x.ARRIVAL_AIRPORT,
+                                                                  cabin_class=x.cabin_class,
+                                                                  currencies=x.currencies), axis=1)
 
 print(amm_test)
 
-print(amm_test["IATA_CODE"]. value_counts())
+# print(f"{(amm_test["IATA_CODE_DEP"]== None).sum()} rows have been returned with no corresponding value")
 
 amm_test.to_csv(r"/Users/chaualala/Desktop/UZH/MSc Geographie/2. Semester/GEO885 - GIS Science Project/GEO885/R/amm_test_emissions.csv", index=False)
 
